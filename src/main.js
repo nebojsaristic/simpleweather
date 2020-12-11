2c61562bfc0e7ce8f06058cae1323948fb0b82d3
Nebojsa <nebojsaristic90@gmail.com>
2020-12-11 17:16:31 +0100

    Forecast feature completed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/*
+var mixin = {
+  data: function () {
+    return 
+      units: "metric"
+  }
+}
+*/
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
